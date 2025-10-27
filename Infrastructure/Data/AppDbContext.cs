c0c233f44db36ee3be2e89bc33275dd60d2f8a52
Brandon Gatewood <brandongatewood9@gmail.com>
2025-10-27 10:02:51 -0700

    Add AppDbContext w user and tip configurations

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using TipJar.Domain.Entities;
+
+namespace TipJar.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tip> Tips { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasMany(u => u.Tips)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.Metadata.FindNavigation(nameof(User.Tips))!
+                .SetPropertyAccessMode(PropertyAccessMode.Field);
+        });
+
+        modelBuilder.Entity<Tip>(entity =>
+        {
+            entity.Property(t => t.Amount)
+                .HasPrecision(18, 2);
+        });
+    }
+}
