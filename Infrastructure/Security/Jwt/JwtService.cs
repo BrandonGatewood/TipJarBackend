5db648d61578eca7192bdb35111bdc5cf9e1af74
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2026-06-05 20:24:28 -0700

    Added documentation to file

--- Infrastructure/Security/Jwt/JwtService.cs
+++ Infrastructure/Security/Jwt/JwtService.cs
@@ -3,15 +3,23 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Options;
-using TipJar.Domain.Interfaces.Services;
+using TipJar.Domain.Interfaces.Security;
 using TipJar.Infrastructure.Security.Jwt;
 
 namespace TipJar.Application.Services;
 
+/// <summary>
+/// Service responsible for generating JWT for authenticated users.
+/// </summary>
 public class JwtService(IOptions<JwtOptions> options) : IJwtService
 {
     private readonly JwtOptions _options = options.Value;
 
+    /// <summary>
+    /// Generates a signed JWT for the specified user.
+    /// </summary>
+    /// <param name="id">The unique identifier of the user.</param>
+    /// <returns>A JWT string representing the authenticated user..</returns>
     public string GenerateToken(Guid id)
     {
         var claims = new[]
@@ -19,9 +27,12 @@ public class JwtService(IOptions<JwtOptions> options) : IJwtService
             new Claim(ClaimTypes.NameIdentifier, id.ToString())
         };
 
+        // Create the signing key from options 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key));
+        // Define signing algorithm and credentials
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
+        // Build the JWT
         var token = new JwtSecurityToken(
             issuer: _options.Issuer,
             audience: _options.Audience,
@@ -30,6 +41,7 @@ public class JwtService(IOptions<JwtOptions> options) : IJwtService
             signingCredentials: creds
         );
 
+        // Serialize token to string format
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
